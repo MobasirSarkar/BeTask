bb813034f8610db0b34782f0778c30bf12dec9bc
MobasirSarkar <mobasirgithubonly@gmail.com>
2024-09-20 00:06:20 +0530

    scrap the nextjs and implement htmx and templ but need to fix the tailwindcss

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,9 @@
+import LoginForm from "./components/LoginForm";
+
 export default function Home() {
    return (
-      <div>
-         Mobasir
+      <div className="max-w-5xl h-full flex items-center justify-center mx-auto border">
+         <LoginForm />
       </div>
    );
 }
